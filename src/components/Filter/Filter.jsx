f0751be209cc01c26e21dfb88af2355dbbc1c724
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-07-12 16:59:25 +0200

    Refactors Filter from class to hook

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,29 +1,25 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types'
 import { Input, Label } from 'components/ContactForm/ContactForm.styled';
 
-export class Filter extends Component {
-  handleChange = e => {
+export const Filter = ({ filter, onChange }) => {
+  const handleChange = e => {
     const { value } = e.target;
-    this.props.onChange(value);
+    onChange(value);
   };
 
-  render() {
-    const { filter } = this.props;
-    return (
-      <>
-        <Label htmlFor="input-search">Find contacts by name</Label>
-        <Input
-          type="text"
-          id="input-search"
-          onChange={this.handleChange}
-          name="filter"
-          value={filter}
-        />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Label htmlFor="input-search">Find contacts by name</Label>
+      <Input
+        type="text"
+        id="input-search"
+        onChange={handleChange}
+        name="filter"
+        value={filter}
+      />
+    </>
+  );
+};
 
 Filter.propTypes = {
   filter: PropTypes.string.isRequired,
